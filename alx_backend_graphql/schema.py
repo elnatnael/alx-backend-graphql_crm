aed6bf146da9c026eca7390616b1a68317a7efd7
elnatnael <meranat9104@gmail.com>
2025-07-17 13:50:21 +0300

    Refactor: use CRMQuery in schema

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,9 +1,15 @@
 import graphene
 
-class Query(graphene.ObjectType):
+# Step 1: Create CRMQuery class
+class CRMQuery(graphene.ObjectType):
     hello = graphene.String()
 
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
 
+# Step 2: Inherit CRMQuery in the main Query class
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+# Step 3: Create the schema
 schema = graphene.Schema(query=Query)
